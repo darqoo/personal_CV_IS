6ccaf2501870050c2d27ca19d1f8984b3ad5c0b0
Darqoo <d.smolag@gmail.com>
2017-10-19 23:19:40 +0200

    fixed problem with display rwd version

---
+++ script/script.js
@@ -0,0 +1,6 @@
+// $(function(){
+//   var body = document.body;
+//   var html = document.documentElement;
+//   var bodyH = Math.max(body.scrollHeight, body.offsetHeight, body.getBoundingClientRect().height, html.clientHeight, html.scrollHeight, html.offsetHeight);
+//   $('body').css('height', bodyH);
+// });
