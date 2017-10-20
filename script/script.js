2283ba3422c257ff1d0ec4d9a0c7aaf62e6908b2
Darqoo <d.smolag@gmail.com>
2017-10-20 22:27:09 +0200

    Modifications have been made to the css file

--- script/script.js
+++ script/script.js
@@ -1,14 +1,9 @@
-(function() {
-    var look = document.getElementById('look');
-    var map = document.getElementById('map');
-    look.addEventListener("click", function() {
-        if (map.style.display == "inline-block") {
-            map.style.display = "none";
-        } else {
-            map.style.display = "inline-block"
-        }
-    });
-
-
-
-})();
+var look = document.getElementById('look');
+var map = document.getElementById('map');
+look.addEventListener("click", function() {
+    if (map.style.display == "inline-block") {
+        map.style.display = "none";
+    } else {
+        map.style.display = "inline-block"
+    }
+});
